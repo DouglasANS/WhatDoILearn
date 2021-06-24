3bc9d81782362ff204ee6a9f76c41b6bfe8ab4f4
DouglasANS <douglasans@hotmail.com>
2021-06-24 07:59:45 -0300

    Visualiza o bd com liveData

--- app/src/main/java/com/example/whatdoilearn/view/MainActivity.kt
+++ app/src/main/java/com/example/whatdoilearn/view/MainActivity.kt
@@ -4,12 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.whatdoilearn.WhatDidILearnApplication
 import com.example.whatdoilearn.databinding.ActivityMainBinding
+import com.example.whatdoilearn.viewmodel.MainViewModel
+import com.example.whatdoilearn.viewmodel.ViewModelFactory
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +25,7 @@ class MainActivity : AppCompatActivity() {
         val adapter = LearnedItemAdapter()
         recyclerView.adapter = adapter
 
-        val repository = (application as WhatDidILearnApplication).repository
-        val items = repository.learnedItems
+        val items = viewModel.learnedItems
 
         items.observe(this, Observer {
             adapter.learnItems = it
@@ -31,12 +35,9 @@ class MainActivity : AppCompatActivity() {
         val adicionar = binding.floatingActionButton
 
         adicionar.setOnClickListener {
-
             val i = Intent(this, new_Learned_item::class.java)
             startActivity(i)
+
         }
-
-
-
     }
 }
